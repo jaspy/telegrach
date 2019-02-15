52e08c625fee4c0a69f426bfac1c7fcb370ea8b2
foobic <foobic@gmail.com>
2019-02-15 14:34:16 +0200

    Marked module added

--- client/src/components/Index.vue
+++ client/src/components/Index.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="Index">
     <p>Hi</p>
+    <textarea v-model="text"></textarea>
+    <div id="result" v-html="compiledMarked">
+      <!-- {{  }} -->
+    </div>
   </div>
 </template>
 
 <script>
+
+
 export default {
   name: 'Index',
+  data(){
+    return {
+      text: ""
+    }
+  },
+  computed: {
+    compiledMarked(){
+      return this.$options.filters.marked(this.text)
+    }
+  }
 }
 </script>
 
--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import marked from 'marked';
+
+Vue.filter('marked', marked)
 
 Vue.config.productionTip = false
 

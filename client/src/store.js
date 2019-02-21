f3287cfba97b23ec10aa574d5ea3cc567484edf2
FMShelbyGT500 <eightynov@gmail.com>
2019-02-21 19:58:54 +0200

    Added axios lib

--- client/src/store.js
+++ client/src/store.js
@@ -2,8 +2,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import marked from 'marked';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 Vue.use(Vuex);
+Vue.use(VueAxios, axios)
 
 const store = new Vuex.Store({
   state: {
